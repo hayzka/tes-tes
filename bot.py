eea00e23046532ab0409bb57441bda0ec99e36c4
hayzka <zakiawardhani13@gmail.com>
2026-04-03 11:32:07 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,14 @@
 import asyncio
+import os
 import string
+import time
 from telethon import TelegramClient, functions
 from telethon.errors import UsernameInvalidError, UsernameOccupiedError, FloodWaitError
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# =====================
-# CONFIG
-# =====================
-import os
+
 
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
@@ -17,31 +16,27 @@ bot_token = os.getenv("BOT_TOKEN")
 
 PASSWORD = "yyy"
 
-SESSIONS = ["acc1", "acc2", "acc3"]
+SESSIONS = ["acc1", "acc2", "acc3", "acc4", "acc5", "acc6", "acc7", "acc8", "acc9", "acc10"]
 
 AUTHORIZED_USERS = set()
 clients = []
-client_index = 0
+client_cooldown = {}
 running_tasks = {}
 
-# =====================
-# INIT CLIENTS
-# =====================
+
 async def init_clients():
     for s in SESSIONS:
         c = TelegramClient(s, api_id, api_hash)
         await c.start()
         clients.append(c)
+        client_cooldown[c] = 0
 
-def get_client():
-    global client_index
-    c = clients[client_index]
-    client_index = (client_index + 1) % len(clients)
-    return c
+def get_available_client():
+    now = time.time()
+    return [c for c in clients if client cooldown[c] <= now]
 
-# =====================
-# AUTH
-# =====================
+#auth
+
 def auth(func):
     async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if update.effective_user.id not in AUTHORIZED_USERS:
@@ -50,9 +45,9 @@ def auth(func):
         return await func(update, context)
     return wrapper
 
-# =====================
-# LOGIN
-# =====================
+
+# login
+
 async def login(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.args and context.args[0] == PASSWORD:
         AUTHORIZED_USERS.add(update.effective_user.id)
@@ -60,9 +55,9 @@ async def login(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         await update.message.reply_text("Salah, yang bener aja")
 
-# =====================
-# VARIATION
-# =====================
+
+# scan
+
 def tambah_huruf(base):
     letters = string.ascii_lowercase
     return list({base[:i] + l + base[i:] for i in range(len(base)+1) for l in letters})
@@ -74,42 +69,106 @@ def ganti_huruf(base):
 def uncommon(base):
     return list({base[:i] + base[i] + base[i:] for i in range(len(base))})
 
-# =====================
-# CHECK FUNCTION
-# =====================
-async def check_usernames(usernames):
-    results = []
+#autokeep
 
-    for username in usernames:
-        client = get_client()
+@auth
+async def keep(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        return await update.message.reply_text("/keep username")
+        
+    username = context.args[0].replace("@", "")
+    user_id = update_effective_user.id
+    if user_id in running_tasks:
+        return await update.message.reply_text("dah running")
+        await update.message.reply_text(f"hunting @{usename}")
 
+async def worker():
+    while true:
+        available = get_available_clients()
+        if not available:
+            await asyncio.sleep(2)
+            continue
+        for client in available:
+            try:
+                ok = await client(functions.account.CheckUsernameRequest(username))
+                
+                if ok:
+                    await client(functions.account.CheckUsernameRequest(username))
+                    await update.message.reply_text(f"dah dikeep @{usename}")
+                    return
+                    
+            except FloodWaitError as e:
+                client_cooldown[client] = time.time + e.second
+            except Exeception:user_id = update_effective_user.id
+    if user_id in running_tasks:
+        return await update.message.reply_text("dah running")
+        user_id = update_effective_user.id
+    if user_id in running_tasks:
+        return await update.message.reply_text("dah running")
+        await update.message.reply_text(f"hunting @{usename}")
+
+                pass
+        
+        await asyncio.sleep(0,5)
+
+running tasks[user_id] = asyncio.create_tasks(worker())
+                
+#stop
+
+@auth
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update_effective_user.id
+    if user_id in running_tasks:
+        running_tasks[user_id].cancel()
+        del running_tasks[user_id]
+        await update.message.reply_text(f"STOP")
+    else:
+         await update.message.reply_text(f"tidak ada tasks")
+         
+
+#
+
+async def check_one(client, usernames):
         try:
             ok = await client(functions.account.CheckUsernameRequest(username))
 
             if ok:
-                results.append(f"🟢 @{username}")
+                return f"🟢 @{username}"
             else:
-                results.append(f"🔴 @{username}")
-
-        except UsernameInvalidError:
-            results.append(f"⚠️ @{username}")
-
-        except UsernameOccupiedError:
-            results.append(f"🔴 @{username}")
+                return f"🔴 @{username}"
 
         except FloodWaitError as e:
-            await asyncio.sleep(e.seconds)
+            client_cooldown[client] = time.time() + e.second
+            return f"⚠️ @{username}"
+
+        except UsernameOccupiedError:
+            return f"🔴 @{username}"
 
         except Exception:
-            results.append(f"❌ @{username}")
+            return f"❌ @{username}"
 
-        await asyncio.sleep(0.3)
+    async def check_username_fast(username):
+        results = []
+        sem = asyncio.Semaphore(5)
+        
+        async def worker(username):
+            async with sem:
+                available = get_available_clients()
+                if not available:
+                    await asyncio.sleep(2)
+                    return f"bntr @{username}"
+                client = available[0]
+                result = await check_one(client, username)
+                await asyncio.sleep(0,3)
+                return result
 
-    return results
+        tasks = [worker(u) for u in username]
+        results = await asyncio.gather(*tasks)
+        
+        return results
 
-# =====================
-# COMMAND CHECK
-# =====================
+#check
+
 @auth
 async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
@@ -120,9 +179,8 @@ async def check(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text("\n".join(result))
 
-# =====================
-# SCAN COMMANDS (AUTO CHECK)
-# =====================
+#scan
+
 @auth
 async def scantamhur(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if not context.args:
@@ -162,9 +220,8 @@ async def scanuncommon(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await update.message.reply_text("\n".join(result))
 
-# =====================
-# MAIN
-# =====================
+#main
+
 async def main():
     await init_clients()
 
@@ -172,6 +229,8 @@ async def main():
 
     app.add_handler(CommandHandler("login", login))
     app.add_handler(CommandHandler("check", check))
+    app.add_handler(CommandHandler("keep", keep))
+    app.add_handler(CommandHandler("stop", stop))
     app.add_handler(CommandHandler("scantamhur", scantamhur))
     app.add_handler(CommandHandler("scanganhur", scanganhur))
     app.add_handler(CommandHandler("scanuncommon", scanuncommon))
@@ -182,13 +241,11 @@ async def main():
     await app.start()
     await app.updater.start_polling()
 
-    # biar tetap jalan
+    
     while True:
         await asyncio.sleep(999)
 
-import asyncio
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
    
