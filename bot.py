82b1c2a68ea6c63d1842c5ef07ed368b8e6aad2f
hayzka <zakiawardhani13@gmail.com>
2026-04-03 10:14:09 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 
-PASSWORD = "kucing"
+PASSWORD = "yyy"
 
 SESSIONS = ["acc1", "acc2", "acc3"]
 
